77f7a9f479828915ef5a603a79708cd0196ffe33
fraancofenoglio <francofeno26@gmail.com>
2023-06-22 13:04:47 -0300

    animacion de flechita y mover footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import FullSearch from "./components/FullSearch";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
@@ -20,33 +21,9 @@ function App() {
       <div></div>
 
       <Sucursales/>
+      <Footer/>
 
-      <footer>
 
-        <div className="footer-links">
-          <a href="/">Inicio</a>
-          <a href="/">Busca tu auto</a>
-          <a href="/">Vende tu auto</a>
-          <a href="/">Contacto</a>
-        </div>
-
-        <h3>Consultanos</h3>
-
-        <div className="form-container-footer">
-          <form>
-            <input type="text" placeholder="Nombre"/>
-
-            <input type="email" placeholder="Email"/>
-
-            <input type="number" placeholder="Teléfono"/>
-
-            <textarea rows="7" cols="40" placeholder="Consulta"></textarea>
-
-            <input type="submit" value="ENVIAR" className="form-footer-button"/>
-          </form>
-        </div>
-
-      </footer>
 
     </>
   );
