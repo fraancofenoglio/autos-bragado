a6f7fa5124892aab9f89104f23094aeedf050174
fraancofenoglio <francofeno26@gmail.com>
2023-07-09 18:29:31 -0300

    filtrado y cards mobile

--- src/pages/Compra.jsx
+++ src/pages/Compra.jsx
@@ -6,74 +6,38 @@ const Compra = () => {
 
     const [minPrice, setMinPrice] = useState(200000);
     const [maxPrice, setMaxPrice] = useState(10000000);
-    const [minYear, setMinYear] = useState(2010);
+    const [minYear, setMinYear] = useState(2000);
     const [maxYear, setMaxYear] = useState(2023);
     const [MARCA, setMARCA] = useState("");
     const [MODELO, setMODELO] = useState("");
     const [COMBUSTIBLE, setCOMBUSTIBLE] = useState("");
     const [KILOMETROS, setKILOMETROS] = useState(100000);
 
-    const [resultados, setResultados] = useState();
+    const [resultados, setResultados] = useState(autos);
 
-    const {marca, modelo, combustible, transmision, motor, color, precio, año, km, descripcion, imagenes } = autos;
+    // const {marca, modelo, combustible, transmision, motor, color, precio, año, km, descripcion, imagenes } = autos;
 
-    console.log(resultados)
+    
+    const marcasUnicas = Array.from(new Set(autos.map((auto) => auto.marca)));
+    const modelosUnicos = Array.from(new Set(autos.filter(auto => auto.marca.toLowerCase() === MARCA).map(auto => auto.modelo)));
 
-    const marcaFilter = (auto) => {
-        if (MARCA) {
-            return auto.marca.toLowerCase() === MARCA 
-        }
-        return auto
-    };
 
-    const modeloFilter = (auto) => {
-        if(MODELO){
-            return auto.modelo.toLowerCase() === MODELO
-        }
-        return auto
-    };
 
-    const combustibleFilter = (auto) => {
-        if(COMBUSTIBLE) {
-            return auto.combustible.toLowerCase() === COMBUSTIBLE
-        }
-        return auto
-    };
+    const marcaFilter = auto => MARCA ? auto.marca.toLowerCase() === MARCA : auto;
 
-    const kmFilter = (auto) => {
-        if (KILOMETROS) {
-            return auto.km <= KILOMETROS
-        }
-        return auto
-    };
+    const modeloFilter = auto => MODELO ? auto.modelo.toLowerCase() === MODELO : auto;
 
-    const precioMinFilter = (auto) => {
-        if (minPrice) {
-            return auto.precio >= minPrice
-        }
-        return auto
-    };
+    const combustibleFilter = (auto) => COMBUSTIBLE ? auto.combustible.toLowerCase() === COMBUSTIBLE : auto;
 
-    const precioMaxFilter = (auto) => {
-        if (maxPrice) {
-            return auto.precio <= maxPrice
-        }
-        return auto
-    };
+    const kmFilter = auto => KILOMETROS ? auto.km <= KILOMETROS : auto;
 
-    const yearMinFilter = (auto) => {
-        if (minYear) {
-            return auto.year >= minYear
-        }
-        return auto
-    };
+    const precioMinFilter = auto => minPrice ? auto.precio >= minPrice : auto;
 
-    const yearMaxFilter = (auto) => {
-        if (maxYear) {
-            return auto.year <= maxYear
-        }
-        return auto
-    };
+    const precioMaxFilter = auto => maxPrice ? auto.precio <= maxPrice : auto;
+
+    const yearMinFilter = auto => minYear ? auto.year >= minYear : auto;
+
+    const yearMaxFilter = (auto) => maxYear ? auto.year <= maxYear : auto;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -89,28 +53,17 @@ const Compra = () => {
 
             <select className="compra-select" onChange={ e => setMARCA(e.target.value.toLowerCase())}>
                 <option value={""}> Todas las marcas </option>
-                <option value={"volkswagen"}>VOLKSWAGEN</option>
-                <option value={"ford"}>FORD</option>
-                <option value={"chevrolet"}>CHEVROLET</option>
-                <option value={"fiat"}>FIAT</option>
-                <option value={"renault"}>RENAULT</option>
-                <option value={"toyota"}>TOYOTA</option>
-                <option value={"citroen"}>CITROEN</option>
-                <option value={"peugeot"}>PEUGEOT</option>
-                <option value={"honda"}>HONDA</option>
-                <option value={"nissan"}>NISSAN</option>
-                <option value={"audi"}>AUDI</option>
-                <option value={"bmw"}>BMW</option>
+                {marcasUnicas.map((marca, i) => <option value={marca} key={i}>{marca}</option>)}
+
             </select>
 
             <select className="compra-select" onChange={ e => setMODELO(e.target.value.toLowerCase())}>
               <option value={""}>Todos los modelos</option>
-              <option value={"corsa"}>Corsa</option>
-              <option value={"cruze"}>Cruze</option>
-              <option value={"s10"}>S10</option>
-              <option value={"tracker"}>Tracker</option>
-              <option value={"astra"}>Astra</option>
-              <option value={"fiesta"}>Fiesta</option>
+
+              {modelosUnicos.map((modelo, i) => (
+                <option value={modelo} key={i}>{modelo}</option>
+            ))}
+
             </select>
 
             <select className="compra-select" onChange={ e => setCOMBUSTIBLE(e.target.value.toLowerCase())}>
@@ -138,28 +91,36 @@ const Compra = () => {
             <input className="rangos" value={maxPrice} type="range" min={200000} max={10000000} step={100000} onChange={(e)=> setMaxPrice(e.target.value)}></input>
 
             <span>Año Mínimo: <strong>{minYear}</strong></span>
-            <input className="rangos" value={minYear} type="range" min={2010} max={2023} step={1} onChange={(e)=> setMinYear(e.target.value)}></input>
+            <input className="rangos" value={minYear} type="range" min={2000} max={2023} step={1} onChange={(e)=> setMinYear(e.target.value)}></input>
 
             <span>Año Máximo: <strong>{maxYear}</strong></span>
-            <input className="rangos" value={maxYear} type="range" min={2010} max={2023} step={1} onChange={(e)=> setMaxYear(e.target.value)}></input>
+            <input className="rangos" value={maxYear} type="range" min={2000} max={2023} step={1} onChange={(e)=> setMaxYear(e.target.value)}></input>
 
             <input className="form-search-btn" type="submit" value="BUSCAR"></input>
 
 
         </form>
 
-        <div>
+        <section className="resultados-busqueda">
             {
-             resultados ?
+             resultados && resultados.length !== 0 ?
                 <>
                 {resultados.map((auto, i) => (
-                    <h3 key={i}>{auto.modelo}</h3>
+                    <div className="card" key={i}>
+                        <img className="card-img" src={auto.imagenes[0]} alt="" />
+                        <h4 className="card-title">{auto.marca + " " + auto.modelo}</h4>
+                        <span>{auto.motor}</span>
+                        <p className="card-info">{`${auto.year} / ${auto.combustible} / ${auto.transmision}`}</p>
+                        <p className="card-km">{` ${auto.km} km`}</p>
+                        <h4 className="card-precio">${auto.precio}</h4>
+                        <button className="card-button">VER</button>
+                    </div>
                     ))
                 }
                 </>
-                : <>nada</>
+                : <></>
             }
-        </div>
+        </section>
     </>
   )
 }
