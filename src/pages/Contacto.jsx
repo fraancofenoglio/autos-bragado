2838206f8008d045005e36472755d3d69a39b7c3
fraancofenoglio <francofeno26@gmail.com>
2023-08-10 19:11:10 -0300

    finished contacto desktop

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -11,6 +11,15 @@ const Contacto = () => {
                 <h4>Contactenos</h4>
             </div>
         </div>
+
+        <div className='redes'>
+            <h4>Nuestra redes sociales:</h4>
+            <div className='redes-container'>
+                <a href="www.instagram.com"><span>Instagram:</span> AutosBragado</a>
+                <a href="www.facebook.com"><span>Facebook: </span> Autos Bragado</a>
+                <a href="mailto:autosbragado@autos.com"><span>Email: </span> autosbragado@autos.com</a>
+            </div>
+        </div>
         <Contact/>
         <Footer></Footer>
     </>
